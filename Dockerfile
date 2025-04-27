cb230c0984f303b718fececf7fe7aa3b4cb5212d
Brajesh Kumar <brajesh.12210249@lpu.in>
2025-04-27 10:13:25 +0530

    remove all files

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM jenkinsci/blueocean:2.332.3-1
-# Add any custom configurations or plugins here
--- app.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# Call the Flask API
-response = requests.get("http://flask-service:5000/api/message")
-if response.status_code == 200:
-    print("Response from Flask API:", response.json()["message"])
-else:
-    print("Failed to connect to Flask API")
